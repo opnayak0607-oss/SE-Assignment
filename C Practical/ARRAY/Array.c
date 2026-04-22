e706ac5250b1f55ee2eb55c6c6ccef80885a5ef2
Omprasad Nayak <opnayak0607@gmail.com>
2026-04-22 09:45:43 +0530

    Delete C Practical directory

--- C Practical/ARRAY/Array.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a[5]={1,2,3,4,5};
-	int i;
-	for(i=0;i<5;i++){
-		printf("\n a[%d]=%d",i,a[i]);
-	}
-}
--- C Practical/do-while.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int i=1;
-	do{
-		printf("\n i=%d",i);
-		i++;
-	}
-	while(i<=10);
-}
